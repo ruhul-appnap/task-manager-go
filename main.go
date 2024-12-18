94298816a4cbcab5e0f0e0bd294d9bd38f7d2625
Ruhul <ruhul@appnap.io>
2024-12-18 17:25:32 +0600

    Add create task endpoint

--- main.go
+++ main.go
@@ -1,13 +1,45 @@
 package main
 
-import ("fmt"
- "log"
- "github.com/gofiber/fiber/v2")
+import (
+	"fmt"
+	"log"
 
-func main()  {
-  fmt.Println("Hello");
+	"github.com/gofiber/fiber/v2"
+)
 
-  app := fiber.New();
+type Task struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
 
-  log.Fatal(app.Listen(":5000"))
+func main() {
+	fmt.Println("Hello WORLD")
+
+	app := fiber.New()
+
+	taskList := []Task{}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"message": "success"})
+	})
+
+	app.Post("/api/tasks", func(c *fiber.Ctx) error {
+		task := &Task{}
+
+		if err := c.BodyParser(task); err != nil {
+			return c.Status(422).JSON(fiber.Map{"message": "Invalid request body", "error": err.Error()})
+		}
+
+		if task.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Body is required"})
+		}
+
+		task.ID = len(taskList) + 1
+		taskList = append(taskList, *task)
+
+		return c.Status(201).JSON(fiber.Map{"message": "success", "data": task})
+	})
+
+	log.Fatal(app.Listen(":5000"))
 }
